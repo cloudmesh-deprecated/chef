02d7d0fe01f89277712c15ed82e726a864ce5d95
jklingin <jklinginsmith@gmail.com>
2013-08-21 05:00:12 -0400

    epel attributes file

---
+++ cookbooks/epel/attributes/default.rb
@@ -0,0 +1,3 @@
+default["epel"]["download_url"] = "http://ftp.linux.ncsu.edu/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm"
+default["epel"]["download_dir"] = "/tmp"
+default["epel"]["checksum"] = "e5ed9ecf22d0c4279e92075a64c757ad2b38049bcf5c16c4f2b75d5f6860dc0d"
