9ecafe4b4d64449836e22c4439c2500bf1e59d84
jklingin <jklinginsmith@gmail.com>
2013-11-06 05:16:47 -0500

    Updated ZeroMQ version

--- cookbooks/zeromq/attributes/default.rb
+++ cookbooks/zeromq/attributes/default.rb
@@ -1,5 +1,5 @@
-default["zeromq"]["version"] = "3.2.3"
+default["zeromq"]["version"] = "3.2.4"
 default["zeromq"]["download_url"] = "http://download.zeromq.org/zeromq-#{node["zeromq"]["version"]}.tar.gz"
 default["zeromq"]["download_dir"] = "/tmp"
-default["zeromq"]["checksum"] = "e134c1aaf829dd77bca9dc0fa199ba31095a581bd0d3dda5fc59523b9f401238"
+default["zeromq"]["checksum"] = "20f7edf3cf323229c5597734f1dee002270542f79a6a59ebcff51dcebf95a7a2"
 default["zeromq"]["prefix"] = "/usr/local"
