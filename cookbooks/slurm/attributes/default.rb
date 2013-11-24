786a424e40a90ad8045dd844f8fa50bf77bd1938
jklingin <jklinginsmith@gmail.com>
2013-11-24 15:21:41 -0500

    Updated SLURM to 2.6.4

--- cookbooks/slurm/attributes/default.rb
+++ cookbooks/slurm/attributes/default.rb
@@ -1,7 +1,7 @@
-default["slurm"]["version"] = "2.6.3"
+default["slurm"]["version"] = "2.6.4"
 default["slurm"]["download_url"] = "http://www.schedmd.com/download/latest/slurm-#{node["slurm"]["version"]}.tar.bz2"
 default["slurm"]["download_dir"] = "/tmp"
-default["slurm"]["checksum"] = "3523f6866531d48061e6b4d769a17e005b7d440bb5cee6c6ab6a12518c884d23"
+default["slurm"]["checksum"] = "f44a9a80c502dba9809127dc2a04069fd7c87d6b1e10824fe254b2077f9adac8"
 default["slurm"]["prefix"] = "/usr/local"
 default["slurm"]["sysconfdir"] = "#{node["slurm"]["prefix"]}/etc"
 
