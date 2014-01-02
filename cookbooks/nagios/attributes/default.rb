072b348ed40c039b8561d5b632a86d222107d59d
Koji Tanaka <kj.tanaka@gmail.com>
2014-01-02 13:37:35 -0500

    minor change on nagios recipe

--- cookbooks/nagios/attributes/default.rb
+++ cookbooks/nagios/attributes/default.rb
@@ -7,6 +7,7 @@ default['nagios']['download_dir'] = '/tmp'
 # NRPE
 default['nagios']['nrpe']['version'] = '2.15'
 default['nagios']['nrpe']['download_url'] = 'http://downloads.sourceforge.net/project/nagios/nrpe-2.x/nrpe-2.15/nrpe-2.15.tar.gz?r=http%3A%2F%2Fexchange.nagios.org%2Fdirectory%2FAddons%2FMonitoring-Agents%2FNRPE--2D-Nagios-Remote-Plugin-Executor%2Fdetails&ts=1388507791&use_mirror=hivelocity'
+default['nagios']['nrpe']['allowed_hosts'] = '127.0.0.1'
 
 # Plugins
 default['nagios']['plugins']['version'] = '1.5'
--- cookbooks/nagios/test/integration/nrpe/serverspec/nrpe_spec.rb
+++ cookbooks/nagios/test/integration/nrpe/serverspec/nrpe_spec.rb
@@ -7,3 +7,9 @@ describe service('nrpe') do
     it { should be_enabled }
     it { should be_running }
 end
+
+cmd = '/opt/nagios/libexec/check_nrpe -H 127.0.0.1 -c check_users'
+describe command(cmd) do
+    expected_stdout = 'USERS OK - 1 users currently logged in |users=1;5;10;0'
+    it { should return_stdout expected_stdout }
+end
