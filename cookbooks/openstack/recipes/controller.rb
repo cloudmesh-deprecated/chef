6a1c59cd5d1b3e3370ba0fbf2f1d6112005c7623
Koji Tanaka <kj.tanaka@gmail.com>
2014-01-14 22:44:18 -0500

    fix adding repo

--- cookbooks/openstack/recipes/controller.rb
+++ cookbooks/openstack/recipes/controller.rb
@@ -18,4 +18,65 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+password = node['openstack']['password']
+admin_password = node['openstack']['admin_password']
+service_password = node['openstack']['service_password']
+mysql_admin_password = node['openstack']['mysql_admin_password']
+mysql_password = node['openstack']['mysql_password']
+rabbit_password = node['openstack']['rabbit_password']
+controller_public_address = node['openstack']['controller_public_address']
+controller_admin_address = node['openstack']['controller_admin_address']
+controller_iternal_address = node['openstack']['controller_internal_address']
+fixed_range = node['openstack']['fixed_range']
+mysql_access = node['openstack']['mysql_access']
+public_interface = node['openstack']['public_interface']
+internal_interface = node['openstack']['internal_interface']
 
+package "ubuntu-cloud-keyring" do
+  action :install
+end
+
+cookbook_file "/etc/apt/sources.list.d/havana.list" do
+  source "havana.list"
+  mode "0644"
+  owner "root"
+  group "root"
+  action :create_if_missing
+end
+
+execute "apt-get-update" do
+  command "apt-get update && apt-get -y dist-upgrade"
+  ignore_failure true
+  action :run
+end
+
+packages = %w[ntp
+              python-mysqldb
+              python-memcache
+              rabbitmq-server
+              mysql-server
+              memcached
+              open-iscsi-utils
+              bridge-utils
+              keystone
+              glance
+              cinder-api
+              cinder-scheduler
+              nova-api
+              nova-cert
+              nova-objectstore
+              nova-network
+              nova-scheduler
+              nova-conductor
+              nova-doc
+              nova-console
+              nova-consoleauth
+              nova-novncproxy
+              novnc
+              openstack-dashboard]
+
+packages.each do |package|
+  package "#{package}" do
+    action :install
+  end
+end
