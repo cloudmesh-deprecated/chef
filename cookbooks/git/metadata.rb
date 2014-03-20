4002f8bda683834598d327e1b834a1b1388b794b
Koji Tanaka <kj.tanaka@gmail.com>
2014-03-20 16:48:29 -0400

    update author

--- cookbooks/git/metadata.rb
+++ cookbooks/git/metadata.rb
@@ -1,8 +1,6 @@
 name             'git'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures cookbooks/git'
+maintainer       'Koji Tanaka'
+maintainer_email 'kj.tanaka@gmail.com'
+description      'Installs/Configures git'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
-
