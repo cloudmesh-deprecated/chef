38227dbad43ffa0361c47e6b8a6300dd9119c5f8
jklingin <jklinginsmith@gmail.com>
2014-05-20 20:34:11 -0400

    Updated serf version

--- cookbooks/serf/attributes/default.rb
+++ cookbooks/serf/attributes/default.rb
@@ -1,5 +1,5 @@
-default["serf"]["version"] = "0.4.5"
+default["serf"]["version"] = "0.6.0"
 default["serf"]["download_url"] = "https://dl.bintray.com/mitchellh/serf/#{node["serf"]["version"]}_linux_amd64.zip"
 default["serf"]["download_dir"] = "/tmp"
-default["serf"]["checksum"] = "85177142ea38c4f3c93f7c6b7361b0a4c3e5a83f4ce5d2477e79247603860eef"
+default["serf"]["checksum"] = "4687532337d85bffe846ffb5e4e16ebea421d7e05c0965dffdb16c94c52"
 default["serf"]["prefix"] = "/usr/local"
