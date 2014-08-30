936b41aaaf703a5084d33d902072ecce954551a4
jklingin <jklinginsmith@gmail.com>
2014-08-30 09:51:55 -0400

    Use get-pip.py to install pip; numpy and scipy versions

--- cookbooks/python/attributes/default.rb
+++ cookbooks/python/attributes/default.rb
@@ -1,12 +1,12 @@
-default["python"]["version"] = "2.7.6"
+default["python"]["version"] = "2.7.8"
 default["python"]["short_version"] = "#{node["python"]["version"][0,3]}"
 default["python"]["download_url"] = "https://www.python.org/ftp/python/#{node["python"]["version"]}/Python-#{node["python"]["version"]}.tgz"
 default["python"]["download_dir"] = "/tmp"
-default["python"]["checksum"] = "99c6860b70977befa1590029fae092ddb18db1d69ae67e8b9385b66ed104ba58"
+default["python"]["checksum"] = "74d70b914da4487aa1d97222b29e9554d042f825f26cb2b93abd20fdda56b557"
 default["python"]["prefix"] = "/usr/local"
 default["python"]["install_method"] = "source"
 
-default["python"]["cython_version"] = "0.20.1"
+default["python"]["cython_version"] = "0.20.2"
 default["python"]["cython_download_url"] = "https://pypi.python.org/packages/source/C/Cython/Cython-#{node["python"]["cython_version"]}.tar.gz"
 default["python"]["cython_checksum"] = "31a563744a21d7b10355f25a3bca96b37ec5d32bdecfc75e93d65a5f7e62766c"
 
@@ -29,20 +29,23 @@ default["python"]["mpi4py_download_url"] = "https://mpi4py.googlecode.com/files/
 default["python"]["mpi4py_checksum"] = "e7bd2044aaac5a6ea87a87b2ecc73b310bb6efe5026031e33067ea3c2efc3507"
 default["python"]["mpi4py_mpi_prefix"] = "/usr/local"
 
-default["python"]["numpy_version"] = "1.8.1"
+default["python"]["numpy_version"] = "1.8.2"
 default["python"]["numpy_download_url"] = "https://pypi.python.org/packages/source/n/numpy/numpy-#{node["python"]["numpy_version"]}.tar.gz"
-default["python"]["numpy_checksum"] = "3d722fc3ac922a34c50183683e828052cd9bb7e9134a95098441297d7ea1c7a9"
+default["python"]["numpy_checksum"] = "6d487fc724780d66746bde264ea71f5cd77d3a39e52ee2b073dcaed63bc669db"
 
 default["python"]["pandas_version"] = "0.13.1"
 default["python"]["pandas_download_url"] = "https://pypi.python.org/packages/source/p/pandas/pandas-#{node["python"]["pandas_version"]}.tar.gz"
 default["python"]["pandas_checksum"] = "6813746caa796550969ed98069f16627f070f6d8d60686cfb3fa0e66c2e0312b"
 
-default["python"]["scipy_version"] = "0.13.3"
-default["python"]["scipy_download_url"] = "http://downloads.sourceforge.net/project/scipy/scipy/#{node["python"]["scipy_version"]}/scipy-#{node["python"]["scipy_version"]}.tar.gz"
-default["python"]["scipy_checksum"] = "a9e33c7ee060c35cd5c6ee3ac9e5e9bbb99219d7ea8c89537c2e80e581670266"
+default["python"]["pip_get-pip_url"] = "https://bootstrap.pypa.io/get-pip.py"
+default["python"]["pip_get-pip_checkum"] = "d43dc33a5670d69dd14a9be1f2b2fa27ebf124ec1b212a47425331040f742a9b"
 
-default["python"]["setuptools_setup_download_url"] = "https://bitbucket.org/pypa/setuptools/raw/bootstrap/ez_setup.py"
-default["python"]["setuptools_setup_checksum"] = "dbbff7092ff05b8399677300cd111029af0cab01b2eff355f9dd982ebafda5d6"
+default["python"]["scipy_version"] = "0.14.0"
+default["python"]["scipy_download_url"] = "http://downloads.sourceforge.net/project/scipy/scipy/#{node["python"]["scipy_version"]}/scipy-#{node["python"]["scipy_version"]}.tar.gz"
+default["python"]["scipy_checksum"] = "4b41a3e6bf178df1c7f0ef3bfeabf1f56610329aca5dbd7b6d64da8ac9af6b14"
+
+default["python"]["setuptools_setup_download_url"] = "https://bootstrap.pypa.io/ez_setup.py"
+default["python"]["setuptools_setup_checksum"] = "97cbf7f590e79613b245d30de7949d57e801ca149aa138a558863be8e29eb18c"
 
 default["python"]["tables_version"] = "3.1.1"
 default["python"]["tables_download_url"] = "https://pypi.python.org/packages/source/t/tables/tables-#{node["python"]["tables_version"]}.tar.gz"
--- cookbooks/python/metadata.rb
+++ cookbooks/python/metadata.rb
@@ -3,4 +3,5 @@ maintainer_email "jklingin@indiana.edu"
 license          "Apache 2.0"
 description      "Installs/Configures python"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+name             "python"
 version          "1.0.0"
