653352415182f2b353c64d6c2ebbc3585efa58e9
jklingin <jklinginsmith@gmail.com>
2014-08-30 09:46:56 -0400

    Added ZeroMQ tarball checksum

--- cookbooks/zeromq/attributes/default.rb
+++ cookbooks/zeromq/attributes/default.rb
@@ -1,5 +1,5 @@
 default["zeromq"]["version"] = "4.0.4"
 default["zeromq"]["download_url"] = "http://download.zeromq.org/zeromq-#{node["zeromq"]["version"]}.tar.gz"
 default["zeromq"]["download_dir"] = "/tmp"
-default["zeromq"]["checksum"] = ""
+default["zeromq"]["checksum"] = "1ef71d46e94f33e27dd5a1661ed626cd39be4d2d6967792a275040e34457d399"
 default["zeromq"]["prefix"] = "/usr/local"
