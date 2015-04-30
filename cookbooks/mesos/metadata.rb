a3ebb347e7b3966215e7d996e3d28908ddd4604a
jklingin <jklinginsmith@gmail.com>
2015-04-29 20:26:34 -0400

    updated email in mesos metadata.rb

--- cookbooks/mesos/metadata.rb
+++ cookbooks/mesos/metadata.rb
@@ -1,6 +1,6 @@
 name             'mesos'
 maintainer       'Jonathan Klinginsmith'
-maintainer_email 'jlingin@indiana.edu'
+maintainer_email 'jklingin@indiana.edu'
 license          'Apache 2.0'
 description      'Installs/Configures mesos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
