6b8c5802f5e91dbfc1398e49de3d0f3564aee678
jklingin <jklinginsmith@gmail.com>
2015-04-29 20:25:27 -0400

    update mesos metadata.rb file

--- cookbooks/mesos/metadata.rb
+++ cookbooks/mesos/metadata.rb
@@ -1,7 +1,7 @@
 name             'mesos'
 maintainer       'Jonathan Klinginsmith'
 maintainer_email 'jlingin@indiana.edu'
-license          'All rights reserved'
-description      'Apache 2.0'
+license          'Apache 2.0'
+description      'Installs/Configures mesos'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
