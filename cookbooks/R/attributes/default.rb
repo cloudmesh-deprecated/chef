6a38c7999feb6ce813e88b3a4e9cb6145ee94354
jklingin <jklinginsmith@gmail.com>
2015-06-20 17:22:08 -0400

    Updating to latest R version

--- cookbooks/R/attributes/default.rb
+++ cookbooks/R/attributes/default.rb
@@ -1,6 +1,6 @@
-default["R"]["version"] = "3.2.0"
+default["R"]["version"] = "3.2.1"
 default["R"]["download_url"] = "http://ftp.ussg.iu.edu/CRAN/src/base/R-3/R-#{node["R"]["version"]}.tar.gz"
 default["R"]["download_dir"] = Chef::Config[:file_cache_path]
-default["R"]["checksum"] = "f5ae953f18ba6f3d55b46556bbbf73441350f9fd22625402b723a2b81ff64f35"
+default["R"]["checksum"] = "d59dbc3f04f4604a5cf0fb210b8ea703ef2438b3ee65fd5ab536ec5234f4c982" 
 default["R"]["prefix"] = "/usr/local"
 default["R"]["configure_options"] = "--prefix=#{node["R"]["prefix"]} --with-x=no --enable-R-shlib"
