d4f0e939f730177e9810a64d245cd80c83a145a0
jklingin <jklinginsmith@gmail.com>
2015-06-22 21:10:48 -0400

    updated HDF5 version

--- cookbooks/hdf5/attributes/default.rb
+++ cookbooks/hdf5/attributes/default.rb
@@ -1,5 +1,5 @@
-default["hdf5"]["version"] = "1.8.12"
+default["hdf5"]["version"] = "1.8.15"
 default["hdf5"]["download_url"] = "http://www.hdfgroup.org/ftp/HDF5/releases/hdf5-#{node["hdf5"]["version"]}/src/hdf5-#{node["hdf5"]["version"]}.tar.bz2"
 default["hdf5"]["download_dir"] = "/tmp"
-default["hdf5"]["checksum"] = "6d080f913a226a3ce390a11d9b571b2d5866581a2aa4434c398cd371c7063639"
+default["hdf5"]["checksum"] = "0003ac76314b9c3dd819f595eb33890f02f6bf000414013a89f9907ac09e3920"
 default["hdf5"]["prefix"] = "/usr/local"
