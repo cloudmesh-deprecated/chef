02ab47f44f80824b0a6e9998df6c868e2e132eea
jklingin <jklinginsmith@gmail.com>
2015-09-06 08:07:29 -0400

    copyright date on python::pymongo

--- cookbooks/python/recipes/pymongo.rb
+++ cookbooks/python/recipes/pymongo.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: python
 # Recipe:: pymongo
 #
-# Copyright 2013, Jonathan Klinginsmith
+# Copyright 2015, Jonathan Klinginsmith
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
