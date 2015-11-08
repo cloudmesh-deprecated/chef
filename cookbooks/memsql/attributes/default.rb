e7d79888740341708d1925edc9e014eae50f968f
jklingin <jklinginsmith@gmail.com>
2015-11-08 16:23:03 -0500

    Update MemSQL bin checksum

--- cookbooks/memsql/attributes/default.rb
+++ cookbooks/memsql/attributes/default.rb
@@ -3,4 +3,4 @@ default['memsql']['download_url'] = "http://download.memsql.com/memsql-ops-#{nod
 default['memsql']['download_dir'] = Chef::Config[:file_cache_path]
 default['memsql']['checksum'] = 'f6bdf8451ce4d8cd79a555accbcbbef09eb828e0c4c12d1c632442a82739466f'
 default['memsql']['bin_download_url'] = 'http://download.memsql.com/releases/latest/memsqlbin_amd64.tar.gz'
-default['memsql']['bin_checksum'] = '3fee740409d1d8ed3f21b45b960d9a9244cc119ae1a59af433613b111428e526'
+default['memsql']['bin_checksum'] = '2bb43d1c719a28dfdbaad7126a5301a8327299984ba6678c76e07f77306ed7ce'
