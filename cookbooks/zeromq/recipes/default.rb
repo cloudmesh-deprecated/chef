f2a463e74a23c2056872e29865401a1fa0499072
jklingin <jklinginsmith@gmail.com>
2015-11-20 14:09:03 -0500

    updated zeromq::default with libsodium-devel

--- cookbooks/zeromq/recipes/default.rb
+++ cookbooks/zeromq/recipes/default.rb
@@ -1,8 +1,8 @@
 #
-# Cookbook Name:: python
+# Cookbook Name:: zeromq
 # Recipe:: default
 #
-# Copyright 2013, Jonathan Klinginsmith
+# Copyright 2015, Jonathan Klinginsmith
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -17,37 +17,37 @@
 # limitations under the License.
 #
 
-zeromq_version = node["zeromq"]["version"]
-zeromq_download_url = node["zeromq"]["download_url"]
-zeromq_download_dir = node["zeromq"]["download_dir"]
-zeromq_checksum = node["zeromq"]["checksum"]
-zeromq_prefix = node["zeromq"]["prefix"]
+zeromq_version = node['zeromq']['version']
+zeromq_download_url = node['zeromq']['download_url']
+zeromq_download_dir = node['zeromq']['download_dir']
+zeromq_checksum = node['zeromq']['checksum']
+zeromq_prefix = node['zeromq']['prefix']
 
 # http://blog.serverdensity.com/building-zeromq-and-pyzmq-on-red-hat/
 # autoconf on RHEL6 -- autoconf2.6x needed.
 # Needed to perform the following for all tests to pass "ulimit -n 1200"
-packages = %w[autoconf gcc-c++ libsodium libtool pkgconfig]
-packages.each do |package|
-  package "#{package}" do
+packages = %w[autoconf gcc-c++ libsodium-devel libtool pkgconfig]
+packages.each do |pkg|
+  package pkg do
     action :install
   end
 end
 
 remote_file "#{zeromq_download_dir}/zeromq-#{zeromq_version}.tar.gz" do
-  source "#{zeromq_download_url}"
-  mode "0644"
-  checksum "#{zeromq_checksum}"
+  source zeromq_download_url
+  mode '0644'
+  checksum zeromq_checksum
 end
 
-execute "untar zeromq tarball" do
+execute 'untar zeromq tarball' do
   command "tar -xzf zeromq-#{zeromq_version}.tar.gz"
-  cwd "#{zeromq_download_dir}"
+  cwd zeromq_download_dir
   creates "#{zeromq_download_dir}/zeromq-#{zeromq_version}"
   action :run
 end
 
-script "install zeromq" do
-  interpreter "bash"
+script 'install zeromq' do
+  interpreter 'bash'
   cwd "#{zeromq_download_dir}/zeromq-#{zeromq_version}"
   code <<-EOH
   ./configure --prefix=#{zeromq_prefix}
