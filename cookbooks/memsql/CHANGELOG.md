54738cca90f1be7525a83361b6850769bab2c503
jklingin <jklinginsmith@gmail.com>
2015-12-13 16:09:42 -0500

    updated memsql binary checksum

--- cookbooks/memsql/CHANGELOG.md
+++ cookbooks/memsql/CHANGELOG.md
@@ -5,7 +5,7 @@ This file is used to list changes made in each version of the memsql cookbook.
 
 0.1.0
 -----
-- [your_name] - Initial release of memsql
+- Jonathan Klinginsmith - Initial release of memsql
 
 - - -
 Check the [Markdown Syntax Guide](http://daringfireball.net/projects/markdown/syntax) for help with Markdown.
--- cookbooks/memsql/attributes/default.rb
+++ cookbooks/memsql/attributes/default.rb
@@ -3,4 +3,4 @@ default['memsql']['download_url'] = "http://download.memsql.com/memsql-ops-#{nod
 default['memsql']['download_dir'] = Chef::Config[:file_cache_path]
 default['memsql']['checksum'] = 'f6bdf8451ce4d8cd79a555accbcbbef09eb828e0c4c12d1c632442a82739466f'
 default['memsql']['bin_download_url'] = 'http://download.memsql.com/releases/latest/memsqlbin_amd64.tar.gz'
-default['memsql']['bin_checksum'] = '2bb43d1c719a28dfdbaad7126a5301a8327299984ba6678c76e07f77306ed7ce'
+default['memsql']['bin_checksum'] = 'c900e586c3f8f3fe52a59a9872a70e0b03d98df3ed7cc9a87229947b7a3e4ab0'
