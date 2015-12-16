5d5a19860c4d85775e952a0c3b509958c455947f
jklingin <jklinginsmith@gmail.com>
2015-12-16 06:07:03 -0500

    Update ZeroMQ from rubocop

--- cookbooks/zeromq/attributes/default.rb
+++ cookbooks/zeromq/attributes/default.rb
@@ -1,5 +1,5 @@
-default["zeromq"]["version"] = "4.1.3"
-default["zeromq"]["download_url"] = "http://download.zeromq.org/zeromq-#{node["zeromq"]["version"]}.tar.gz"
-default["zeromq"]["download_dir"] = Chef::Config[:file_cache_path]
-default["zeromq"]["checksum"] = "61b31c830db377777e417235a24d3660a4bcc3f40d303ee58df082fcd68bf411"
-default["zeromq"]["prefix"] = "/usr/local"
+default['zeromq']['version'] = '4.1.3'
+default['zeromq']['download_url'] = "http://download.zeromq.org/zeromq-#{node['zeromq']['version']}.tar.gz"
+default['zeromq']['download_dir'] = Chef::Config[:file_cache_path]
+default['zeromq']['checksum'] = '61b31c830db377777e417235a24d3660a4bcc3f40d303ee58df082fcd68bf411'
+default['zeromq']['prefix'] = '/usr/local'
--- cookbooks/zeromq/metadata.rb
+++ cookbooks/zeromq/metadata.rb
@@ -1,6 +1,7 @@
-maintainer       "Jonathan Klinginsmith"
-maintainer_email "jklingin@indiana.edu"
-license          "All rights reserved"
-description      "Installs/Configures ZeroMQ"
+name 'zeromq'
+maintainer 'Jonathan Klinginsmith'
+maintainer_email 'jklingin@indiana.edu'
+license 'Apache 2.0'
+description 'Installs/Configures ZeroMQ'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version '0.1.0'
